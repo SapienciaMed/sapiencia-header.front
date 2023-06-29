362bc8a258dd570ada56a32206270d35d650f38c
Daniel Flores <dflores@i4digital.com>
2023-06-29 18:21:07 -0500

    ajustes env

--- src/common/hooks/auth-service.hook.ts
+++ src/common/hooks/auth-service.hook.ts
@@ -4,17 +4,23 @@ import { ApiResponse } from "../utils/api-response";
 import useCrudService from "./crud-service.hook";
 
 export function useAuthService() {
-  const baseURL: string = "http://localhost:4201";
+  const baseURL: string = process.env.urlApiAuth;
   const authUrl: string = "/api/v1/auth";
 
   const { get } = useCrudService(null, baseURL);
 
-  async function getAuthorization(token: string): Promise<ApiResponse<IAuthorization>> {
+  async function getAuthorization(
+    token: string
+  ): Promise<ApiResponse<IAuthorization>> {
     try {
       const endpoint: string = `/authorization/get-by-token/${token}`;
       return await get(`${authUrl}${endpoint}`);
     } catch (error) {
-      return new ApiResponse( {} as IAuthorization , EResponseCodes.FAIL, "Error no controlado");
+      return new ApiResponse(
+        {} as IAuthorization,
+        EResponseCodes.FAIL,
+        "Error no controlado"
+      );
     }
   }
 
